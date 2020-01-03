1393fea650f7fcb3f6c29e1b2911df5ba577f130
Martin Stiles <martsst@stud.ntnu.no>
2020-01-03 18:19:27 +0100

    Updates

---
+++ precache-manifest.79a023cf8a54a599ee729ea766143b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fc953da38710243afab6c949d5db7f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bc96d7d29ed9efe1405d",
+    "url": "/monsters-rolodex/static/css/main.ea5fa824.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc96d7d29ed9efe1405d",
+    "url": "/monsters-rolodex/static/js/main.d8820111.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
